f0c083cf83fb019dbbb0011c6d565fc266ca6130
Pascal Grimaud <pascalgrimaud@gmail.com>
2016-12-27 23:19:03 +0100

    Add compose file for jhipster-registry and a gateway

--- _travis/run-tests.sh
+++ _travis/run-tests.sh
@@ -10,10 +10,22 @@ if [[ "$APP" == "cassandra/2.1.15" ]]; then
     ./dcqlsh -e "select * from system.schema_keyspaces"
     ./dcqlsh -k test -e "select * from user"
 
-elif [[ $APP == "mysql/5.7.13" ]]; then
+elif [[ "$APP" == "mysql/5.7.13" ]]; then
     cd "$APP"
     docker-compose up -d
     sleep 30
     docker-compose logs
 
+elif [[ "$APP" == "jhipster-registry" ]]; then
+    cd "$APP"
+    docker-compose up -d
+    sleep 30
+    docker-compose logs
+    curl -v http://localhost:8761
+
+    cd ../jhipster-gateway
+    docker-compose up -d
+    sleep 30
+    docker-compose logs
+    curl -v http://localhost:8088
 fi
---
+++ jhipster-registry/README.md
@@ -0,0 +1,19 @@
+# jhipster-registry
+
+Pull the image:
+- `docker pull jhipster/jhipster-registry`
+
+Start jhipster-registry:
+- `docker-compose up`
+
+Start jhipster-registry in background
+- `docker-compose up -d`
+
+Stop jhipster-registry:
+- `docker-compose stop`
+
+All configuration are under the folder:
+- `central-config`
+
+Note:
+- the `eureka.client.serviceUrl.defaultZone` is not defined by Spring Config Server
